358ef66ac325dac152c16a6ea968a979f839bc2d
Ajmain Fayek <ajmainfayek733@gmail.com>
2024-12-26 20:43:20 +0600

    Simple 404 page created

--- src/Layouts/Error.jsx
+++ src/Layouts/Error.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Error = () => {
-    return <div>Error</div>;
+    return (
+        <div className="mt-24 flex-col flex space-y-4 text-gray-400 text-4xl text-center content-center">
+            <span className="font-bold">404 Not Found...</span>{" "}
+            <Link className="text-dark font-semibold hover:underline text-lg">
+                Back to Home
+            </Link>
+        </div>
+    );
 };
 
 export default Error;
